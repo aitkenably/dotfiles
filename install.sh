9023c78ae99bad22b6f700ea495ba46fc16ebe99
Don Aitken <don.aitken@gmail.com>
2019-06-05 21:51:09 -0400

    Switched to zsh. Updated install script to create zsh links.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Function: timestamp
 # Create a timestamp in the form: 2017-05-06_18:29:44
@@ -8,7 +8,7 @@ function timestamp {
 
 # Function: backup_file <filename>
 # Backup up a file by copying it with an appended timestamp
-function backup_file {
+function backupFile {
     if [ -f "$1" ]; then
         local ts=$(timestamp) 
         cp "$1" "$1.bak.$ts" 
@@ -18,15 +18,17 @@ function backup_file {
 # Function: replace_file <current_file> <new_file> 
 # Makes a backup of <current_file> and then replaces 
 # it with a symbolic link to <new_file>
-function replace_file {
-    if [ -f "$1" ] && [ -f "$2" ]; then
-        backup_file "$1"
-        rm "$1" 
+function installFile {
+    if [ -f "$2" ]; then
+        if [ -f "$1" ]; then
+            backupFile "$1"
+            rm "$1" 
+        fi
         ln -s "$2" "$1"
     fi
 }
 
-replace_file ~/.bash_profile ~/.dotfiles/bash_profile
-replace_file ~/.bashrc ~/.dotfiles/bashrc
-replace_file ~/.vimrc ~/.dotfiles/vimrc
-replace_file ~/.gitconfig ~/.dotfiles/gitconfig
+installFile ~/.vimrc ~/.dotfiles/vimrc
+installFile ~/.zshrc ~/.dotfiles/zshrc
+
+#replace_file ~/.gitconfig ~/.dotfiles/gitconfig
---
+++ zshrc
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.history
+
+alias l='ls -GFh'
+alias rm='rm -i'
+alias mv='mv -i'
+alias cls='clear'
+
+
