e663cf99cfecebadc9f5de55658ab35c3b120856
Don Aitken <don.aitken@gmail.com>
2020-08-07 18:53:40 -0400

    Added dotfiles update function

--- bash_aitkenably.sh
+++ bash_aitkenably.sh
@@ -7,3 +7,8 @@ alias l="ls -lG"
 alias cls="clear"
 
 export PS1="\n\u@\H in \w\n[\d \@] \! > "
+
+# Upgrade dotfiles by reruning install script 
+function dotfiles {
+    wget -q -O - https://raw.githubusercontent.com/aitkenably/dotfiles/master/install.sh | bash   
+}
