48da3a4b0d01d84d956590562bf1b462b9ea327e
Don Aitken <don.aitken@gmail.com>
2020-08-07 19:21:53 -0400

    Added additional aliases

--- bash_aitkenably.sh
+++ bash_aitkenably.sh
@@ -1,11 +1,22 @@
+
+# Basic aliases 
 alias mv="mv -i"
 alias cp="cp -i"
 alias rm="rm -i"
-
-alias l="ls -lG"
-
+alias l="ls -lGFh"
 alias cls="clear"
+alias v='view'
 
+# Print $PATH one entry per line 
+alias path="echo $PATH | tr : '\n'"
+
+# Git
+alias ga='git add'
+alias gs='git status'
+alias gc='git commit'
+alias gm='git push origin master'
+
+# Set prompt
 export PS1="\n\u@\H in \w\n[\d \@] \! > "
 
 # Upgrade dotfiles by reruning install script 
