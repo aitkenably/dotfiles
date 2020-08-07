91e0ccaba59b8ad9ad1402cd596cacc9afefc044
Don Aitken <don.aitken@gmail.com>
2020-08-07 18:40:39 -0400

    Renamed bash script with .sh ending for syntax highlighting

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ echo "Installing dotfiles from GitHub"
 echo ""
 
 echo "Downloading .bash_aitkenably..."
-wget -q -O ~/.bash_aitkenably https://raw.githubusercontent.com/aitkenably/dotfiles/master/bash_aitkenably 
+wget -q -O ~/.bash_aitkenably.sh https://raw.githubusercontent.com/aitkenably/dotfiles/master/bash_aitkenably.sh 
  
 
 echo ""
 echo "Now complete your installation"
 echo "Add the following to your ~/.bashrc: "  
-echo -e "\t[ -f ~/.bash_aitkenably ] && . ~/.bash_aitkenably"  
+echo -e "\t[ -f ~/.bash_aitkenably.sh ] && . ~/.bash_aitkenably.sh"  
